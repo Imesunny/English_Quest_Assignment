f4f49064219f667e377494c76a1d9c0f37378c7c
Sunny Anand <rvrahulvermaonly1@gmail.com>
2024-02-04 11:39:46 +0530

    Middleware sorted, according to the creator role

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoDBConnection = require("./config/db");
+const morgan = require("morgan");
+const { checkToken, checkRole } = require("./middlewares/middleware");
+const UserRouter = require("./routes/user.routes");
+const BookRouter = require("./routes/book.routes");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length]")
+);
+// console.log(app.use(morgan("combined")), 'Logger Information')
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from backend" });
+  });
+
+
+
+
+
+app.use("/user", UserRouter);
+app.use("/books", BookRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await mongoDBConnection;
+    console.log(`Server running on port ${PORT}`);
+  } catch (error) {
+    console.log("error while listening on port");
+  }
+});
